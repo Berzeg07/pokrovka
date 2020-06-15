7b17877283aa9046e0c1c3444424d0436cfd8c7a
betokov <betokov93@mail.ru>
2020-06-15 17:38:58 +0300

    Сверстан 1й блок карточки товара

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-
+	$('.level-selection-tab li').click(function(e) {
+		e.preventDefault();
+		$('li').removeClass('level-selection-active');
+		$(this).addClass('level-selection-active');
+		var tab = $(this).attr('data-num');
+		$('.level-selection-tab__box').not(tab).css({'display':'none'});
+		$(tab).fadeIn(400);
+	});
+	$('.level-selection-tab li:first').click();
 });
