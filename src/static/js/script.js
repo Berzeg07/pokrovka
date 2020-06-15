52b84bfcb26a18bb55b88969b75fa93a94e2ad93
Aslan <berezg86@gmail.com>
2020-06-15 11:58:21 +0300

    Футер

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('.burger').click(function(){
         $(this).toggleClass('is-active');
-        $('.dropdown-menu, .bg-dropdown').toggleClass('is-active');
+        $('.dropdown-menu, .bg-dropdown, .bg-center').toggleClass('is-active');
     });
 
     $('.close-menu').click(function(){
-        $('.dropdown-menu, .bg-dropdown, .burger').removeClass('is-active');
+        $('.dropdown-menu, .bg-dropdown, .burger, .bg-center').removeClass('is-active');
     });
 
 
