d15394245784052eec50a5ca6615a45ea122f915
betokov <betokov93@mail.ru>
2020-06-17 17:48:07 +0300

    Добавлен блок аппартаменты

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -8,4 +8,38 @@ $(document).ready(function(){
 		$(tab).fadeIn(400);
 	});
 	$('.level-selection-tab li:first').click();
+
+	$('.appartaments-tab a').click(function(e) {
+		e.preventDefault();
+		$('a').removeClass('active');
+		$(this).addClass('active');
+		var tab = $(this).attr('href');
+		$('.appartaments-tab__box').not(tab).css({'display':'none'});
+		$(tab).fadeIn(400);
+	});
+	$('.appartaments-tab a:first').click();
+});
+
+
+var swiperApps1 = new Swiper('.appartaments-tab1', {
+	slidesPerView: 1.29,
+	slidesPerView: 1,
+	spaceBetween: 10,
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'fraction',
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	breakpoints: {
+    // when window width is >= 320px
+    992: {
+    	slidesPerView: 1,
+    	spaceBetween: 30
+    },
+}
+
 });
